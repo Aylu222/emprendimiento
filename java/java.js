4d95069ec035aaadc7edff30be47763b6110ff05
Aylu222 <163070573+Aylu222@users.noreply.github.com>
2024-06-07 17:44:04 -0300

    java

---
+++ java/java.js
@@ -0,0 +1,20 @@
+// elento String. Inserta el texto que vos decidas.
+const text = ("Esta empresa esta orgullosa de decir que somos la N°1 mundialmente, contamos con");
+
+// elemento number
+const number = 200;
+// ESTAS DOS CORRESPONDEN AL PUNTO 1.
+
+// elemento dom
+const comment = document.createTextNode("Sirius Group");
+
+// elemento fecha y hora. Inserta la fecha y hora en el "encabezado".
+const date = new Date("2024/06/06 22:07:20");
+console.log(date)
+//ESTAS DOS CORRESPONDEN AL PUNTO 2. 
+
+console.log(text, number, "sucursales por todo el mundo. Gracias al apoyo que recibimos de nuestra gente dia a dia desde que comenzamos nuestro emprendimiento. Les agradece con amor ", comment);
+// Este "console.log" imprime text, number y comment.
+
+
+
